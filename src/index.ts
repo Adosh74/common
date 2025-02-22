9d710cee3fffc49b22cf58a13f8c4aeb3dcf2807
Mohamed Shebl <hamdysheblm@gmail.com>
2025-02-22 16:42:59 +0200

    updates

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ interface Color {
     red: number;
     green: number;
     blue: number;
+    yellow?: number;
 }
 
 const color: Color = {
     red: 10,
     green: 10,
-    blue: 10
+    blue: 10,
+    yellow: 10
 }
 
 console.log(color);
