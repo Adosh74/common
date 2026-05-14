6458b90a696a44109fdf03104c692296be682890
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-05-14 23:17:47 +0300

    1.0.20

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -8,7 +8,7 @@ interface Event {
 
 abstract class Listener <T extends Event> {
     abstract subject: T['subject'];
-    abstract groupName: string;
+    abstract queueGroupName: string;
     abstract onMessage(data: T['data'], msg: Message): void;
     private client: Stan;
     protected ackWait: number = 5 * 1000;
@@ -23,18 +23,18 @@ abstract class Listener <T extends Event> {
             .setDeliverAllAvailable()
             .setManualAckMode(true)
             .setAckWait(this.ackWait)
-            .setDurableName(this.groupName);
+            .setDurableName(this.queueGroupName);
     }
 
     listen() {
         const subscription = this.client.subscribe(
             this.subject,
-            this.groupName,
+            this.queueGroupName,
             this.subscriptionOptions()
         )
 
         subscription.on('message', (msg: Message) => {
-            console.log(`Message received: ${this.subject} / ${this.groupName}`);  
+            console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);  
 
             const parsedMessage = this.parseMessage(msg)
             this.onMessage(parsedMessage, msg)
